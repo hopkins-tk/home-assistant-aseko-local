ffe849ebbfe7e4ad5efec2c36e982ab0ef4124f4
Enrica <e.ruedin@guggach.com>
2026-04-30 14:52:58 +0000

    fix: ruff lint error

--- custom_components/aseko_local/__init__.py
+++ custom_components/aseko_local/__init__.py
@@ -84,7 +84,9 @@ async def async_setup_entry(
 
         rd.device_discovered = True  # set early to prevent concurrent calls
         try:
-            await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+            await hass.config_entries.async_forward_entry_setups(
+                config_entry, PLATFORMS
+            )
         except Exception:
             rd.device_discovered = False  # allow retry on next device callback
             raise
